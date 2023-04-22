7522898cc70cb51c25aca893df5507ea9814b2ae
MerryDev <svgr1808@outlook.de>
2023-04-22 22:20:42 +0200

    Add missing implementation

--- scoreboard/src/main/java/eu/syplex/scoreboard/Team.kt
+++ scoreboard/src/main/java/eu/syplex/scoreboard/Team.kt
@@ -2,20 +2,97 @@ package eu.syplex.scoreboard
 
 import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
-import org.bukkit.Color
+import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
 import org.bukkit.scoreboard.Scoreboard
+import org.bukkit.scoreboard.Team
 import java.util.*
 
-open class Team(var name: String, private var display: Component, var color: Color, private val scoreboard: AbstractScoreboard) {
+open class Team(var name: String, private var display: Component, private val scoreboard: AbstractScoreboard) {
 
-    private val entities: List<UUID> = ArrayList()
+    private val entities: ArrayList<UUID> = ArrayList()
 
-    fun refresh(bukkitScoreboard: Scoreboard) {}
+    fun refresh() {
+        if (scoreboard is PersonalScoreboard) {
+            for (uuid in entities) {
+                val player = Bukkit.getPlayer(uuid) ?: continue
 
-    fun refresh() {}
+                refresh(player.scoreboard)
+            }
+        } else {
+            refresh((scoreboard as GlobalScoreboard).toBukkitScoreboard())
+        }
+    }
 
-    fun removePlayer(player: Player) {}
+    fun refresh(bukkitScoreboard: Scoreboard) {
+        val team = toBukkitTeam(bukkitScoreboard)
+
+        for (uuid in entities) {
+            val player = Bukkit.getPlayer(uuid)
+
+            if (player != null && !team.hasEntry(player.name)) {
+                team.addEntry(player.name)
+
+            } else if (player == null) {
+                if (team.hasEntry(uuid.toString())) continue
+                team.addEntry(uuid.toString())
+            }
+
+        }
+    }
+
+    fun toBukkitTeam(bukkitScoreboard: Scoreboard): Team {
+        return if (bukkitScoreboard.getTeam(name) != null) bukkitScoreboard.getTeam(name)!! else bukkitScoreboard.registerNewTeam(name)
+    }
+
+    fun addPlayer(player: Player) {
+        addEntity(player)
+    }
+
+    fun addEntity(entity: Entity) {
+        addEntity(entity.uniqueId)
+    }
+
+    fun addEntity(uuid: UUID) {
+        if (entities.contains(uuid)) return
+
+        entities.add(uuid)
+        refresh()
+    }
+
+    fun removePlayer(player: Player) {
+        if (!isOnTeam(player.uniqueId)) return
+        removeEntity(player)
+    }
+
+    fun removeEntity(entity: Entity) {
+        if (!isOnTeam(entity.uniqueId)) return
+
+        removeEntity(entity.uniqueId)
+        handleRemoval(entity.uniqueId.toString())
+    }
+
+    fun removeEntity(uuid: UUID) {
+        entities.remove(uuid)
+        refresh()
+    }
+
+    private fun handleRemoval(entry: String) {
+        if (scoreboard is GlobalScoreboard) {
+            val team = toBukkitTeam(scoreboard.toBukkitScoreboard())
+            team.removeEntry(entry)
+            return
+        }
+
+        val personalScoreboard = scoreboard as PersonalScoreboard
+        for (uuid in personalScoreboard.activePlayers()) {
+            val player = Bukkit.getPlayer(uuid) ?: continue
+            val scoreboard = personalScoreboard.toBukkitScoreboard(player) ?: continue
+            val team = toBukkitTeam(scoreboard) ?: continue
+
+            team.removeEntry(entry)
+        }
+    }
 
 
     /**
@@ -29,13 +106,32 @@ open class Team(var name: String, private var display: Component, var color: Col
                 team.unregister()
             }
         } else {
-//            val scoreboard = ((eu.syplex.scoreboard.GlobalScoreboard) this.scoreboard).toBukkitScoreboard()
-
+            val scoreboard = (this.scoreboard as GlobalScoreboard).toBukkitScoreboard()
+            scoreboard.getTeam(name)?.unregister()
         }
+
+        entities.clear()
     }
 
     fun scoreboard(): AbstractScoreboard {
         return scoreboard
+    }
+
+    fun name(name: String) {
+        if (name.length > 16) throw IllegalArgumentException("The name of the team cannot be longer than 16 chars! (${name.length} present)")
+        this.name = name
+    }
+
+    fun name(): String {
+        return name
+    }
+
+    fun display(): Component {
+        return display
+    }
+
+    fun display(display: Component) {
+        this.display = display
     }
 
     fun entities(): List<UUID> {
