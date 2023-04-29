53128a42a5c903a351a40614dace721790ddb03c
MerryDev <svgr1808@outlook.de>
2023-04-29 21:28:06 +0200

    Add persistent data modification to item creation

--- common/src/main/java/eu/syplex/common/item/ItemCreator.java
+++ common/src/main/java/eu/syplex/common/item/ItemCreator.java
@@ -3,9 +3,11 @@ package eu.syplex.common.item;
 import eu.syplex.common.Builder;
 import net.kyori.adventure.text.TextComponent;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -16,97 +18,115 @@ import org.jetbrains.annotations.NotNull;
  */
 public abstract class ItemCreator implements Builder<ItemStack> {
 
-    /**
-     * The item that will be created
-     */
-    protected final ItemStack itemStack;
+	/**
+	 * The item that will be created
+	 */
+	protected final ItemStack itemStack;
 
-    /**
-     * The meta of the item
-     */
-    protected final ItemMeta itemMeta;
+	/**
+	 * The meta of the item
+	 */
+	protected final ItemMeta itemMeta;
 
-    /**
-     * Instantiates a new {@link ItemCreator} object. This will create a new {@link ItemStack} with the given {@link Material}, the amount of one
-     * and initializes the {@link ItemMeta} from it to be able to modify it later.
-     *
-     * @param material The material
-     */
-    protected ItemCreator(@NotNull Material material) {
-        this.itemStack = new ItemStack(material);
-        this.itemMeta = itemStack.getItemMeta();
-    }
+	/**
+	 * Instantiates a new {@link ItemCreator} object. This will create a new {@link ItemStack} with the given {@link Material}, the amount of one
+	 * and initializes the {@link ItemMeta} from it to be able to modify it later.
+	 *
+	 * @param material The material
+	 */
+	protected ItemCreator(@NotNull Material material) {
+		this.itemStack = new ItemStack(material);
+		this.itemMeta = itemStack.getItemMeta();
+	}
 
-    /**
-     * Instantiates a new {@link ItemCreator} object. This will create a new {@link ItemStack} with the given {@link Material} and amount.
-     * After that the {@link ItemMeta} gets initialized to be able to modify it later.
-     *
-     * @param material The material
-     * @param amount   The amount
-     */
-    protected ItemCreator(@NotNull Material material, int amount) {
-        this.itemStack = new ItemStack(material, amount);
-        this.itemMeta = itemStack.getItemMeta();
-    }
+	/**
+	 * Instantiates a new {@link ItemCreator} object. This will create a new {@link ItemStack} with the given {@link Material} and amount.
+	 * After that the {@link ItemMeta} gets initialized to be able to modify it later.
+	 *
+	 * @param material The material
+	 * @param amount   The amount
+	 */
+	protected ItemCreator(@NotNull Material material, int amount) {
+		this.itemStack = new ItemStack(material, amount);
+		this.itemMeta = itemStack.getItemMeta();
+	}
 
-    /**
-     * Sets the displayed name of the item using a {@link TextComponent}.
-     *
-     * @param component The component
-     * @return This current object instance
-     */
-    public abstract ItemCreator name(@NotNull TextComponent component);
+	/**
+	 * Sets the displayed name of the item using a {@link TextComponent}.
+	 *
+	 * @param component The component
+	 *
+	 * @return This current object instance
+	 */
+	public abstract ItemCreator name(@NotNull TextComponent component);
 
-    /**
-     * Defines, how many items the stack will have. Note, that the maximum is 64 because of dump implementations from Mojang.
-     *
-     * @param amount The amount
-     * @return This current object instance
-     */
-    public abstract ItemCreator amount(int amount);
+	/**
+	 * Defines, how many items the stack will have. Note, that the maximum is 64 because of dump implementations from Mojang.
+	 *
+	 * @param amount The amount
+	 *
+	 * @return This current object instance
+	 */
+	public abstract ItemCreator amount(int amount);
 
-    /**
-     * Sets the displayed text in the item's information box. Each {@link TextComponent} represents a single line. Note, that an empty line is created by calling {@code Component.empty()}.
-     *
-     * @param components The components
-     * @return This current object instance
-     */
-    public abstract ItemCreator lore(@NotNull TextComponent... components);
+	/**
+	 * Sets the displayed text in the item's information box. Each {@link TextComponent} represents a single line. Note, that an empty line is created by calling {@code Component.empty()}.
+	 *
+	 * @param components The components
+	 *
+	 * @return This current object instance
+	 */
+	public abstract ItemCreator lore(@NotNull TextComponent... components);
 
-    /**
-     * Removes the entire displayed text in the items' information box.
-     *
-     * @return This current object instance
-     */
-    public abstract ItemCreator removeLore();
+	/**
+	 * Removes the entire displayed text in the items' information box.
+	 *
+	 * @return This current object instance
+	 */
+	public abstract ItemCreator removeLore();
 
-    /**
-     * Enchants the item with a specific {@link Enchantment} and level. This implementation only allows the default values that are currently implemented in the game.
-     * Note: To break the games' laws and enchant with a higher level than the maximum defaults, please use {@link #enchantUnsafe(Enchantment, int)}
-     *
-     * @param enchantment The enchantment
-     * @param level       The level
-     * @return This current object instance
-     */
-    public abstract ItemCreator enchant(@NotNull Enchantment enchantment, int level);
+	/**
+	 * Enchants the item with a specific {@link Enchantment} and level. This implementation only allows the default values that are currently implemented in the game.
+	 * Note: To break the games' laws and enchant with a higher level than the maximum defaults, please use {@link #enchantUnsafe(Enchantment, int)}
+	 *
+	 * @param enchantment The enchantment
+	 * @param level       The level
+	 *
+	 * @return This current object instance
+	 */
+	public abstract ItemCreator enchant(@NotNull Enchantment enchantment, int level);
 
-    /**
-     * Enchants the item with a specific {@link Enchantment} and level. This implementation allows you to ignore level restrictions.
-     *
-     * @param enchantment The enchantment
-     * @param level       The level
-     * @return This current object instance
-     */
-    public abstract ItemCreator enchantUnsafe(@NotNull Enchantment enchantment, int level);
+	/**
+	 * Enchants the item with a specific {@link Enchantment} and level. This implementation allows you to ignore level restrictions.
+	 *
+	 * @param enchantment The enchantment
+	 * @param level       The level
+	 *
+	 * @return This current object instance
+	 */
+	public abstract ItemCreator enchantUnsafe(@NotNull Enchantment enchantment, int level);
 
-    /**
-     * {@inheritDoc}
-     *
-     * @see Builder
-     */
-    @Override
-    public ItemStack build() {
-        itemStack.setItemMeta(itemMeta);
-        return itemStack;
-    }
+	/**
+	 * Adds persistent data to the item mapped to the {@link NamespacedKey}. Those are nbt-data.
+	 *
+	 * @param key  The mapping key
+	 * @param type The {@link PersistentDataType} to set
+	 * @param data The data to set
+	 * @param <T>  The datatype of the data
+	 * @param <Z>  The datatype of the data
+	 *
+	 * @return The current object instance
+	 */
+	public abstract <T, Z> ItemCreator persistentData(@NotNull NamespacedKey key, @NotNull PersistentDataType<T, Z> type, @NotNull Z data);
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @see Builder
+	 */
+	@Override
+	public ItemStack build() {
+		itemStack.setItemMeta(itemMeta);
+		return itemStack;
+	}
 }
--- item/src/main/java/eu/syplex/item/ItemBuilder.java
+++ item/src/main/java/eu/syplex/item/ItemBuilder.java
@@ -3,8 +3,10 @@ package eu.syplex.item;
 import eu.syplex.common.item.ItemCreator;
 import net.kyori.adventure.text.TextComponent;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
@@ -18,76 +20,85 @@ import java.util.Arrays;
  */
 public class ItemBuilder extends ItemCreator {
 
-    /**
-     * Instantiates this class using {@link ItemCreator#ItemCreator(Material)}.
-     *
-     * @param material The material
-     */
-    public ItemBuilder(Material material) {
-        super(material);
-    }
+	/**
+	 * Instantiates this class using {@link ItemCreator#ItemCreator(Material)}.
+	 *
+	 * @param material The material
+	 */
+	public ItemBuilder(Material material) {
+		super(material);
+	}
 
-    /**
-     * Instantiates this class using {@link ItemCreator#ItemCreator(Material, int)}.
-     *
-     * @param material The material
-     * @param amount   the amount
-     */
-    public ItemBuilder(@NotNull Material material, int amount) {
-        super(material, amount);
-    }
+	/**
+	 * Instantiates this class using {@link ItemCreator#ItemCreator(Material, int)}.
+	 *
+	 * @param material The material
+	 * @param amount   the amount
+	 */
+	public ItemBuilder(@NotNull Material material, int amount) {
+		super(material, amount);
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public ItemCreator name(@NotNull TextComponent component) {
-        itemMeta.displayName(component);
-        return this;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ItemCreator name(@NotNull TextComponent component) {
+		itemMeta.displayName(component);
+		return this;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public ItemCreator amount(int amount) {
-        itemStack.setAmount(amount);
-        return this;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ItemCreator amount(int amount) {
+		itemStack.setAmount(amount);
+		return this;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public ItemCreator lore(@NotNull TextComponent... components) {
-        itemMeta.lore(Arrays.asList(components));
-        return this;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ItemCreator lore(@NotNull TextComponent... components) {
+		itemMeta.lore(Arrays.asList(components));
+		return this;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public ItemCreator removeLore() {
-        itemStack.lore(null);
-        return this;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ItemCreator removeLore() {
+		itemStack.lore(null);
+		return this;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public ItemCreator enchant(@NotNull Enchantment enchantment, int level) {
-        itemMeta.addEnchant(enchantment, level, false);
-        return this;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ItemCreator enchant(@NotNull Enchantment enchantment, int level) {
+		itemMeta.addEnchant(enchantment, level, false);
+		return this;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public ItemCreator enchantUnsafe(@NotNull Enchantment enchantment, int level) {
-        itemMeta.addEnchant(enchantment, level, true);
-        return this;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ItemCreator enchantUnsafe(@NotNull Enchantment enchantment, int level) {
+		itemMeta.addEnchant(enchantment, level, true);
+		return this;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public <T, Z> ItemCreator persistentData(@NotNull NamespacedKey key, @NotNull PersistentDataType<T, Z> type, @NotNull Z data) {
+		itemMeta.getPersistentDataContainer().set(key, type, data);
+		return this;
+	}
 }
